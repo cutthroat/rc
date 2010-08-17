e71ce7fe4403a8ef8b47faf346ad5c1b3e802ef0
Dimitar Dimitrov <silent.cutthroat@gmail.com>
2010-08-17 22:55:05 +0300

    Xdefaults and xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,33 @@
+import XMonad
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Themes
+import XMonad.Util.EZConfig
+
+myLayouts = avoidStruts $ smartBorders $ tiled ||| Mirror tiled ||| noBorders Full ||| tabbed shrinkText (theme smallClean) 
+  where
+     tiled   = Tall nmaster delta ratio
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 3/100
+
+myManageHooks = maybeToDefinite (isFullscreen -?> doFullFloat)
+
+myConfig = defaultConfig
+    { layoutHook = myLayouts
+
+    , terminal = "urxvt"
+
+    , borderWidth = 2
+    , normalBorderColor = "#333333"
+    , focusedBorderColor = "#666666"
+
+    , manageHook = myManageHooks }
+
+main = xmonad =<< dzen (withUrgencyHook dzenUrgencyHook myConfig)
+
+-- nice colour is #cd8b00
